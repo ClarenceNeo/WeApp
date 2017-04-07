8d3b676ebb3618a82299547d75cc91daa717e73f
ClarenceNeo <niuzhiyuan94@gmail.com>
2017-04-07 23:22:46 +0800

    layout

--- Study/pages/index/index.js
+++ Study/pages/index/index.js
@@ -3,24 +3,42 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    imgUrls: [
+      '../../images/banner-1.jpg',
+      '../../images/banner-2.jpg',
+      '../../images/banner-1.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    listItems: [{
+      "title":"第一课：小程序入门...",
+      "content":"第一课：小程序入门...第一课：小程序入门...第一课：小程序入门...",
+      "foot":"主讲老师：nzy"
+    },{
+      "title":"第一课：小程序入门...",
+      "content":"第一课：小程序入门...第一课：小程序入门...第一课：小程序入门...",
+      "foot":"主讲老师：nzy"
+    },{
+      "title":"第一课：小程序入门...",
+      "content":"第一课：小程序入门...第一课：小程序入门...第一课：小程序入门...",
+      "foot":"主讲老师：nzy"
+    },{
+      "title":"第一课：小程序入门...",
+      "content":"第一课：小程序入门...第一课：小程序入门...第一课：小程序入门...",
+      "foot":"主讲老师：nzy"
+    },{
+      "title":"第一课：小程序入门...",
+      "content":"第一课：小程序入门...第一课：小程序入门...第一课：小程序入门...",
+      "foot":"主讲老师：nzy"
+    },{
+      "title":"第一课：小程序入门...",
+      "content":"第一课：小程序入门...第一课：小程序入门...第一课：小程序入门...",
+      "foot":"主讲老师：nzy"
+    }]
   },
   onLoad: function () {
     console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
 })
